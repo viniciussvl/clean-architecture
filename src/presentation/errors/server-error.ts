20cb13f73ee7a10b996edf3829a3cdef5c0a1668
Vinicius Aquino <viniciussvl@hotmail.com>
2022-07-04 11:29:57 -0300

    refactor: move all error imports to a signle file

--- src/presentation/errors/server-error.ts
+++ src/presentation/errors/server-error.ts
@@ -1,4 +1,4 @@
-export default class ServerError extends Error {
+export class ServerError extends Error {
     constructor () {
         super('Internal Server Error')
         this.name = 'ServerError'
