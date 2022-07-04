36a7d9f57713b6dc47956cbf38bf20c3c643acdf
Vinicius Aquino <viniciussvl@hotmail.com>
2022-07-04 14:46:32 -0300

    test: ensure DbAddAccount returns an account on sucess

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -108,4 +108,22 @@ describe('DbAddAccount Usecase', () => {
         const promise = sut.execute(accountData)
         expect(promise).rejects.toThrow()
     })
+
+    test('should return an account on success', async () => {
+        const { sut } = makeSut()
+        const accountData = {
+            name: 'valid_name',
+            email: 'valid_email',
+            password: 'valid_password'
+        }
+
+        const account = await sut.execute(accountData)
+
+        expect(account).toEqual({
+            id: 'valid_id',
+            name: 'valid_name',
+            email: 'valid_email',
+            password: 'hashed_password'
+        })
+    })
 })
