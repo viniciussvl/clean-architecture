1555a5b50bcdabeb55967e470f2f7766519ad58a
Vinicius Aquino <viniciussvl@hotmail.com>
2022-07-05 15:39:26 -0300

    ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, serverError } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { EmailValidator } from '../signup/signup-protocols'
 
@@ -25,7 +25,7 @@ export class LoginController implements Controller {
                 return badRequest(new InvalidParamError('email'))
             }
         } catch (error) {
-
+            return serverError(error)
         }
     }
 }
