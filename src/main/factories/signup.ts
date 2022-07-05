d809f45aa12427a888dc13598b9dd902f6e02f71
Vinicius Aquino <viniciussvl@hotmail.com>
2022-07-05 11:22:19 -0300

    feat: add LogControllerDecorator Decorator Pattern with correct value

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,13 +3,15 @@ import { EmailValidatorAdapter } from '../../utilts/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
 import { AccountRepository } from '../../infra/db/mongodb/account-repository/account'
+import { LogControllerDecorator } from '../decorators/log'
+import { Controller } from '../../presentation/protocols'
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
     const salt = 12
     const emailValidatorAdapter = new EmailValidatorAdapter()
     const accountRepository = new AccountRepository()
     const bcryptAdapter = new BcryptAdapter(salt)
     const dbAddAccount = new DbAddAccount(bcryptAdapter, accountRepository)
     const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
-    return signUpController
+    return new LogControllerDecorator(signUpController)
 }
