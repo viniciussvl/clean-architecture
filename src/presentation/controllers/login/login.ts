761ab5ac9250c87c37d25fad6ea413d900943610
Vinicius Aquino <viniciussvl@hotmail.com>
2022-07-05 15:09:41 -0300

    feat: create LoginController and validate email, password, return badRequest

---
+++ src/presentation/controllers/login/login.ts
@@ -0,0 +1,19 @@
+import { MissingParamError } from '../../errors'
+import { badRequest } from '../../helpers/http-helper'
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+
+export class LoginController implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            if(!httpRequest.body.email) {
+                return badRequest(new MissingParamError('email'))
+            }
+
+            if(!httpRequest.body.password) {
+                return badRequest(new MissingParamError('password'))
+            }
+        } catch (error) {
+
+        }
+    }
+}
