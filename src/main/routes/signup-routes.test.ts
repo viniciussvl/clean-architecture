976c34110d3c59c83a9d40ec6bf0c2c52b3a733d
Vinicius Aquino <viniciussvl@hotmail.com>
2022-07-05 10:10:28 -0300

    refactor: connect to mongodb before integration tests signup routes

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,22 @@
 import request from 'supertest'
+import { AccountRepository } from '../../infra/db/mongodb/account-repository/account'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
 describe('Signup Routes', () => {
+    beforeAll(async () => {
+        await MongoHelper.connect(global.__MONGO_URI__)
+    })
+
+    afterAll(async () => {
+        await MongoHelper.disconnect()
+    })
+
+    beforeEach(async () => {
+        const accountCollection = MongoHelper.getCollection('accounts')
+        await accountCollection.deleteMany({})
+    })
+
     it('should return an account on success', async () => {
         await request(app)
             .post('/api/signup')
@@ -12,5 +27,20 @@ describe('Signup Routes', () => {
                 passwordConfirmation: '123456'
             })
             .expect(200)
+    })
+
+    test('should return an account on success', async () => {
+        const sut = new AccountRepository()
+        const account = await sut.add({
+            name: 'Nome Sobrenome',
+            email: 'nomesobrenom@hotmail.com',
+            password: '123'
+        })
+
+        expect(account).toBeTruthy()
+        expect(account.id).toBeTruthy()
+        expect(account.name).toBe('Nome Sobrenome')
+        expect(account.email).toBe('nomesobrenom@hotmail.com')
+        expect(account.password).toBe('123')
     })
 })
