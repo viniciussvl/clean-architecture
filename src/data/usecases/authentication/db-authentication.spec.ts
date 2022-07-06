5e350f19086ba6f65bf7432d6d98277fed111159
Vinicius Aquino <viniciussvl@hotmail.com>
2022-07-06 08:54:34 -0300

    test: ensure LoadAccountByEmailRepository should return null if email not exists

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -56,4 +56,12 @@ describe('DbAuthentication ', () => {
 
         await expect(promise).rejects.toThrow()
     })
+
+    test('should return null if LoadAccountByEmailRepository returns null', async () => {
+        const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+        jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(null)
+        const accessToken = await sut.auth(makeFakeAuthentication())
+
+        expect(accessToken).toBeNull()
+    })
 })
