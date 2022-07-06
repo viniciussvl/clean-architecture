77a23937f947e548eb089052d091cafb35bb1894
Vinicius Aquino <viniciussvl@hotmail.com>
2022-07-06 11:44:50 -0300

    test: should throw if compare throws BcryptAdapter

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -65,4 +65,15 @@ describe('Bcrypt Adapter', () => {
         const isValid = await sut.compare('any_value', 'any_hash')
         expect(isValid).toBe(false)
     })
+
+    test('should throw if compare throws', async () => {
+        const sut = makeSut()
+        jest.spyOn(bcrypt, 'compare')
+            .mockImplementationOnce(() => {
+                return new Promise((resolve, reject) => reject(new Error()))
+            })
+
+        const promise = sut.compare('any_value', 'any_hash')
+        await expect(promise).rejects.toThrow()
+    })
 })
