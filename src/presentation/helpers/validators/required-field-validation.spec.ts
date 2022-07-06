9f52b640860e3e66204ea0d151aca2527622b67f
Vinicius Aquino <viniciussvl@hotmail.com>
2022-07-05 21:36:48 -0300

    test: makeSut required field validation

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,16 +1,20 @@
 import { MissingParamError } from '../../errors'
 import { RequiredFieldValidation } from './required-field-validation'
 
+const makeSut = (): RequiredFieldValidation => {
+    return new RequiredFieldValidation('field')
+}
+
 describe('RequiredField Validation', () => {
     test('should return a MissingParamError if validation fails', () => {
-        const sut = new RequiredFieldValidation('field')
+        const sut = makeSut()
         const error = sut.validate({ name: 'nome qualquer' })
         expect(error).toEqual(new MissingParamError('field'))
     })
 
     test('should not return if validation succeeds', () => {
-        const sut = new RequiredFieldValidation('name')
-        const error = sut.validate({ name: 'nome qualquer' })
+        const sut = makeSut()
+        const error = sut.validate({ field: 'nome qualquer' })
         expect(error).toBeFalsy()
     })
 })
