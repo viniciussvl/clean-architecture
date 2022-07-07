ab35e95f62f219df5b1d261e8b56ddc01ac5dbbc
Vinicius Aquino <viniciussvl@hotmail.com>
2022-07-07 09:06:20 -0300

    test: should return 401 when login fails

--- src/main/routes/auth-routes.test.ts
+++ src/main/routes/auth-routes.test.ts
@@ -1,8 +1,10 @@
+import { hash } from 'bcrypt'
+import { Collection } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
-let accountCollection
+let accountCollection: Collection
 
 describe('Auth Routes', () => {
     beforeAll(async () => {
@@ -29,6 +31,35 @@ describe('Auth Routes', () => {
                     passwordConfirmation: '123456'
                 })
                 .expect(200)
+        })
+    })
+
+    describe('POST /login', () => {
+        test('should return 200 on login', async () => {
+            const hashedPassword = await hash('123', 12)
+            await accountCollection.insertOne({
+                name: 'rodrigo',
+                email: 'vinicius@hotmail.com',
+                password: hashedPassword
+            })
+
+            await request(app)
+                .post('/api/login')
+                .send({
+                    email: 'vinicius@hotmail.com',
+                    password: '123'
+                })
+                .expect(200)
+        })
+
+        test('should return 401 when login fails', async () => {
+            await request(app)
+                .post('/api/login')
+                .send({
+                    email: 'vinicius@hotmail.com',
+                    password: '12345'
+                })
+                .expect(401)
         })
     })
 })
