858361c680cf8aef652b87c931c105f126f20a79
Vinicius Aquino <viniciussvl@hotmail.com>
2022-07-08 20:24:41 -0300

    test: ensure AuthMiddleware returns 403 if LoadAccountByToken returns null

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -3,13 +3,7 @@ import { AccessDeniedError } from '../errors'
 import { AuthMiddleware } from './auth-middleware'
 import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
 import { AccountModel } from '../../domain/models/account'
-
-const makeFakeAccount = (): AccountModel => ({
-    id: 'valid_id',
-    name: 'valid_name',
-    email: 'valid_email@email.com',
-    password: 'hashed_password'
-})
+import { HttpRequest } from '../protocols'
 
 interface SutTypes {
     sut: AuthMiddleware,
@@ -35,6 +29,19 @@ const makeLoadAccountByToken = (): LoadAccountByToken => {
     return new LoadAccountByTokenStub()
 }
 
+const makeFakeAccount = (): AccountModel => ({
+    id: 'valid_id',
+    name: 'valid_name',
+    email: 'valid_email@email.com',
+    password: 'hashed_password'
+})
+
+const makeFakeRequest = (): HttpRequest => ({
+    headers: {
+        'x-access-token': 'any_token'
+    }
+})
+
 describe('Auth Middleware', () => {
     test('Should return 403 if no x-access-token exists in headers', async () => {
         const { sut } = makeSut()
@@ -47,12 +54,16 @@ describe('Auth Middleware', () => {
         const { sut, loadAccountByTokenStub } = makeSut()
         const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
 
-        await sut.handle({
-            headers: {
-                'x-access-token': 'any_token'
-            }
-        })
+        await sut.handle(makeFakeRequest())
 
         expect(loadSpy).toHaveBeenCalledWith('any_token')
+    })
+
+    test('Should return 403 if LoadAccountByToken returns null', async () => {
+        const { sut, loadAccountByTokenStub } = makeSut()
+        jest.spyOn(loadAccountByTokenStub, 'load').mockReturnValueOnce(new Promise(resolve => resolve(null)))
+        const httpResponse = await sut.handle({})
+
+        expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
     })
 })
