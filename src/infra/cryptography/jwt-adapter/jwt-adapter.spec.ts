79321bbe5da7d84f3856a41970d92b856ca3f4b2
Vinicius Aquino <viniciussvl@hotmail.com>
2022-07-08 21:50:54 -0300

    test: ensure JwtAdappter throws if verify throws

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -58,5 +58,15 @@ describe('Jwt Adapter', () => {
 
             expect(value).toEqual('any_value')
         })
+
+        test('should throw if verify throws', async () => {
+            const sut = makeSut()
+            jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {
+                throw new Error()
+            })
+
+            const promise = sut.decrypt('any_token')
+            await expect(promise).rejects.toThrow()
+        })
     })
 })
