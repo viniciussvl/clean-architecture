bd371b2a8a62500d95e5061d76ff0fd389f3dca3
Vinicius Aquino <viniciussvl@hotmail.com>
2022-07-15 21:49:58 -0300

    feat: swagger config

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import setupMiddleware from './middlewares'
 import setupRoutes from './routes'
+import setupSwagger from './swagger'
 
 const app = express()
+setupSwagger(app)
 setupMiddleware(app)
 setupRoutes(app)
 export default app
---
+++ src/main/docs/index.ts
@@ -0,0 +1,9 @@
+export default {
+    openapi: '3.0.0',
+    info: {
+        title: 'Minha API',
+        description: 'eaeae',
+        version: '1'
+    }
+
+}
