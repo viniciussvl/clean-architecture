cb19cc43e1a9de737af151ef6de85f1308041451
Vinicius Aquino <viniciussvl@hotmail.com>
2022-07-15 21:39:27 -0300

    refactor: remove duplication

--- src/data/usecases/account/add-account/db-add-account.spec.ts
+++ src/data/usecases/account/add-account/db-add-account.spec.ts
@@ -1,7 +1,7 @@
 import { Hasher, AddAccountRepository, LoadAccountByEmailRepository } from './db-add-account-procotols'
 import { DbAddAccount } from './db-add-account'
 import { mockAccountModel, mockAddAccountParams, throwError } from '@/domain/test'
-import { mockAddAccountRepository, mockHasher } from '@/data/test'
+import { mockAddAccountRepository, mockHasher, mockLoadAccountByEmailRepository } from '@/data/test'
 
 type SutTypes = {
     sut: DbAddAccount,
@@ -11,9 +11,10 @@ type SutTypes = {
 }
 
 const makeSut = (): SutTypes => {
+    const loadAccountByEmailRepositoryStub = mockLoadAccountByEmailRepository()
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'loadByEmail').mockReturnValue(new Promise(resolve => resolve(null)))
     const hasherStub = mockHasher()
     const addAccountRepositoryStub = mockAddAccountRepository()
-    const loadAccountByEmailRepositoryStub = makeLoadAccountByEmailRepository()
     const sut = new DbAddAccount(hasherStub, addAccountRepositoryStub, loadAccountByEmailRepositoryStub)
 
     return {
@@ -22,16 +23,6 @@ const makeSut = (): SutTypes => {
         addAccountRepositoryStub,
         loadAccountByEmailRepositoryStub
     }
-}
-
-const makeLoadAccountByEmailRepository = (): LoadAccountByEmailRepository => {
-    class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
-        async loadByEmail (email: string): Promise<any> {
-            return new Promise(resolve => resolve(null))
-        }
-    }
-
-    return new LoadAccountByEmailRepositoryStub()
 }
 
 describe('DbAddAccount Usecase', () => {
